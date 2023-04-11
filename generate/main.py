f1f55a67eec2c5a2a6c3d0450cd78f7c152bfa82
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2023-04-11 21:39:49 +0200

    Correct function generate services and call this function

--- generate/main.py
+++ generate/main.py
@@ -114,8 +114,8 @@ def generate_services(faker: Faker):
         title = key
         description = value
         price = random.randint(500, 5000)
-        cursor.execute(f"""INSERT INTO Services (service_id, title, description, price)
-                          VALUES ({id}, {key}, {description}, {price})
+        cursor.execute(f"""INSERT INTO "Services" (service_id, title, description, price)
+                          VALUES ({id}, '{title}', '{description}', '{price}')
                         """)
         connection.commit()
 
@@ -144,7 +144,8 @@ def generate_workers(faker: Faker):
 
 def main():
     faker = Faker(locale='ru_RU')
-    generate_workers(faker)
+    # generate_workers(faker)
+    generate_services(faker)
 
 if __name__ == "__main__":
     main()
